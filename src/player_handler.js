be747bba766d1df302436d3fa2a4f79984cdb2dd
hightouch <hightouch@hotmail.fr>
2019-02-13 18:17:18 +0100

    fixed balance update

--- src/player_handler.js
+++ src/player_handler.js
@@ -70,18 +70,17 @@ const player_handler = {
             const differenceprod = now.getTime() - user.last_update.getTime();
             var drugs_balance =user.drugs_balance + Number(parseFloat((differenceprod / 1000) * user.drug_production_rate).toFixed(2));
             var alcohols_balance =user.alcohols_balance + Number(parseFloat((differenceprod / 1000) * user.alcohol_production_rate).toFixed(2));
-            var weapons_balance = user.weapons_balance + Number(parseFloat((differenceprod / 1000) * user.weapon_production_rate).toFixed(0));
+            var weapons_balance = user.weapons_balance + Number(parseFloat((differenceprod / 1000) * user.weapon_production_rate).toFixed(2));
             var d_cap = player_handler.calculateCap(buildings,'drug_storage')
             var w_cap = player_handler.calculateCap(buildings,'weapon_storage')
             var a_cap = player_handler.calculateCap(buildings,'alcohol_storage')
-           
             if (buildings.filter(item => item.building === 'operation_center')[0]) {
               const operation_center = buildings.filter(
                 item => item.building === 'operation_center',
               )[0];
-              drugs_balance = drugs_balance + (drugs_balance * (operation_center.lvl * 0.005))
-              weapons_balance = weapons_balance + (weapons_balance* (operation_center.lvl * 0.005))
-              alcohols_balance = alcohols_balance + (alcohols_balance* (operation_center.lvl * 0.005))
+              drugs_balance = drugs_balance + (Number(parseFloat((differenceprod / 1000) * user.drug_production_rate).toFixed(2)) * (operation_center.lvl * 0.005))
+              weapons_balance = weapons_balance + (Number(parseFloat((differenceprod / 1000) * user.weapon_production_rate).toFixed(2))* (operation_center.lvl * 0.005))
+              alcohols_balance = alcohols_balance + (Number(parseFloat((differenceprod / 1000) * user.alcohol_production_rate).toFixed(2))* (operation_center.lvl * 0.005))
               console.log(`applied bonus %${operation_center.lvl * 0.005}`);
             }
             if(drugs_balance>d_cap)
